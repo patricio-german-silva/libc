a65ab0b58a9e3ce29176b61b4dbbe45aab3b8dc2
elpatosilva <elpatosilva@gmail.com>
2024-01-15 01:57:08 -0300

    Speck stream cipher module speck_sc added to lib, also speck block cipher was fixed and it pases the NSA tests

--- dev/speck_cipher/test.c
+++ dev/speck_cipher/test.c
@@ -1,6 +1,5 @@
 #include "stdio.h"
-//#include "speck.h"
-#include "speck_nsa.h"
+#include "speck_sc.h"
 
 
 typedef union{
@@ -16,12 +15,15 @@ typedef union{
 int main(){
    _k K = {0x00 , 0x01 , 0x02 , 0x03 , 0x08 , 0x09 , 0x0a , 0x0b , 0x10 , 0x11 , 0x12 , 0x13 , 0x18 , 0x19 , 0x1a , 0x1b};
    _t pt = {0x2d , 0x43 , 0x75 , 0x74 , 0x74 , 0x65 , 0x72 , 0x3b};
+   _t ptt = {0x2d , 0x43 , 0x75 , 0x74 , 0x74 , 0x65 , 0x72 , 0x3b};
+   _t pttt = {0x2d , 0x43 , 0x75 , 0x74 , 0x74 , 0x65 , 0x72 , 0x3b};
    _t ct;
+   _t ctt;
+   _t cttt;
    _t nt;
-   //_speck s;
-    uint32_t rk[27];
 
-
+/*
+   uint32_t rk[27];
    Speck64128KeySchedule(K.u32, rk);
    for(uint8_t i = 0; i< 27; i++) printf("rk[%d] = %08x\n", i, rk[i]);
    for(uint8_t i = 0; i< 16; i++) printf("K[%d] = %02x\n", i, K.u8[i]);
@@ -32,13 +34,14 @@ int main(){
    printf("DECIPHED: %02x %02x %02x %02x %02x %02x %02x %02x\n" , nt.u8[0], nt.u8[1], nt.u8[2], nt.u8[3], nt.u8[4], nt.u8[5], nt.u8[6], nt.u8[7]);
    return 0;
 
+*/
 
 
 
 /*
-
+   _speck s;
    speck_init(&s, K.u32);
-   for(uint8_t i = 0; i< 27; i++) printf("rk[%d] = %08x\n", i, s.kb[i]);
+   for(uint8_t i = 0; i< 27; i++) printf("rk[%d] = %08x\n", i, s.rk[i]);
    for(uint8_t i = 0; i< 16; i++) printf("K[%d] = %02x\n", i, K.u8[i]);
 
    printf("ORIGINAL: %02x %02x %02x %02x %02x %02x %02x %02x\n" , pt.u8[0], pt.u8[1], pt.u8[2], pt.u8[3], pt.u8[4], pt.u8[5], pt.u8[6], pt.u8[7]);
@@ -47,5 +50,28 @@ int main(){
    speck_decrypt(&s, ct.u32, nt.u32);
    printf("DECIPHED: %02x %02x %02x %02x %02x %02x %02x %02x\n" , nt.u8[0], nt.u8[1], nt.u8[2], nt.u8[3], nt.u8[4], nt.u8[5], nt.u8[6], nt.u8[7]);
    return 0;
-   */
+*/
+
+
+   _speck_sc s;
+   uint32_t I[2] = {0x01234567, 0x89abcdef};
+   speck_sc_init(&s, K.u32, I);
+   for(uint8_t i = 0; i< 27; i++) printf("rk[%d] = %08x\n", i, s.rk[i]);
+   for(uint8_t i = 0; i< 16; i++) printf("K[%d] = %02x\n", i, K.u8[i]);
+
+   printf("ORIGINAL: %02x %02x %02x %02x %02x %02x %02x %02x\n" , pt.u8[0], pt.u8[1], pt.u8[2], pt.u8[3], pt.u8[4], pt.u8[5], pt.u8[6], pt.u8[7]);
+   speck_sc_encrypt(&s, pt.u32, ct.u32);
+   printf("CIPHED: %02x %02x %02x %02x %02x %02x %02x %02x\n" , ct.u8[0], ct.u8[1], ct.u8[2], ct.u8[3], ct.u8[4], ct.u8[5], ct.u8[6], ct.u8[7]);
+   speck_sc_encrypt(&s, ptt.u32, ctt.u32);
+   printf("CIPHED: %02x %02x %02x %02x %02x %02x %02x %02x\n" , ctt.u8[0], ctt.u8[1], ctt.u8[2], ctt.u8[3], ctt.u8[4], ctt.u8[5], ctt.u8[6], ctt.u8[7]);
+   speck_sc_encrypt(&s, pttt.u32, cttt.u32);
+   printf("CIPHED: %02x %02x %02x %02x %02x %02x %02x %02x\n" , cttt.u8[0], cttt.u8[1], cttt.u8[2], cttt.u8[3], cttt.u8[4], cttt.u8[5], cttt.u8[6], cttt.u8[7]);
+   speck_sc_init(&s, K.u32, I);
+   speck_sc_decrypt(&s, ct.u32, nt.u32);
+   printf("DECIPHED: %02x %02x %02x %02x %02x %02x %02x %02x\n" , nt.u8[0], nt.u8[1], nt.u8[2], nt.u8[3], nt.u8[4], nt.u8[5], nt.u8[6], nt.u8[7]);
+   speck_sc_decrypt(&s, ctt.u32, nt.u32);
+   printf("DECIPHED: %02x %02x %02x %02x %02x %02x %02x %02x\n" , nt.u8[0], nt.u8[1], nt.u8[2], nt.u8[3], nt.u8[4], nt.u8[5], nt.u8[6], nt.u8[7]);
+   speck_sc_decrypt(&s, cttt.u32, nt.u32);
+   printf("DECIPHED: %02x %02x %02x %02x %02x %02x %02x %02x\n" , nt.u8[0], nt.u8[1], nt.u8[2], nt.u8[3], nt.u8[4], nt.u8[5], nt.u8[6], nt.u8[7]);
+   return 0;
 }
---
+++ lib/speck_sc.c
@@ -0,0 +1,77 @@
+#include "speck_sc.h"
+
+
+/*
+ * Initializes a speck context for stream encription
+ * given a 128-bit key, extend it for 27 rounds
+ */
+void speck_sc_init(_speck_sc *s, const uint32_t K[4], const uint32_t I[2]){
+	uint32_t D = K[3];
+  uint32_t C = K[2];
+  uint32_t B = K[1];
+  uint32_t A = K[0];
+  s->iv[0] = I[0];
+  s->iv[1] = I[1];
+	for(uint8_t i = 0 ; i < 27 ;){
+		s->rk[i]=A;
+		B=(B>>8 | B<<24);
+		B+=A;
+		B^=i++;
+		A=(A<<3 | A>>29);
+		A^=B;
+
+		s->rk[i]=A;
+		C=(C>>8 | C<<24);
+		C+=A;
+		C^=i++;
+		A=(A<<3 | A>>29);
+		A^=C;
+
+		s->rk[i]=A;
+		D=(D>>8 | D<<24);
+		D+=A;
+		D^=i++;
+		A=(A<<3 | A>>29);
+		A^=D;
+	}
+}
+
+
+
+/*
+ * encrypt a 64 bit block of a stream with an expanded key
+ */
+void speck_sc_encrypt(_speck_sc *s, uint32_t const pt[2], uint32_t ct[2]){
+	ct[0]=(pt[0] ^ s->iv[0]);
+  ct[1]=(pt[1] ^ s->iv[1]);
+	for(uint8_t i = 0; i < 27;){
+		ct[1]=(ct[1]>>8 | ct[1]<<24);
+		ct[1]+=ct[0];
+		ct[1]^=s->rk[i++];
+		ct[0]=(ct[0]<<3 | ct[0]>>29);
+		ct[0]^=ct[1];
+	}
+  s->iv[0] = ct[0];
+  s->iv[1] = ct[1];
+}
+
+
+
+/*
+ * decrypt a 64 bit block of a stream with an expanded key
+ */
+void speck_sc_decrypt(_speck_sc *s, uint32_t const ct[2], uint32_t pt[2]){
+	pt[0]=ct[0];
+  pt[1]=ct[1];
+	for(int8_t i = 26 ; i >= 0;){
+		pt[0]^=pt[1];
+		pt[0]=(pt[0]>>3 | pt[0]<<29);
+		pt[1]^=s->rk[i--];
+		pt[1]-=pt[0];
+		pt[1]=(pt[1]<<8) | (pt[1]>>24);
+	}
+  pt[0] ^= s->iv[0];
+  pt[1] ^= s->iv[1];
+  s->iv[0] = ct[0];
+  s->iv[1] = ct[1];
+}
