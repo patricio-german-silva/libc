56107fe8785e53c2f2e9b752656f6951fd9ab688
elpatosilva <elpatosilva@gmail.com>
2024-01-21 02:54:12 -0300

    some test on fatfs

--- dev/fatfs/src/main.c
+++ dev/fatfs/src/main.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include "ff.h"
 #include "diskio.h"
+#include "strutil.h"
 
 
 
@@ -17,12 +18,18 @@ int main(int argc, char *argv[]){
     char line[100]; /* Line buffer */
     FRESULT fr;     /* FatFs return code */
     BYTE work[FF_MAX_SS]; /* Work area (larger is better for processing time) */
+	 MKFS_PARM opts;
+	 opts.fmt = FM_FAT32;
+	 opts.n_fat = 2;
+	 opts.align = 0;
+	 opts.au_size = 0;
+	 opts.n_root = 0;
 
-
-//	 printf("Create filesystem\n");
+	 //printf("Create filesystem\n");
     /* Format the default drive with default parameters */
-//    fr = f_mkfs("", 0, work, sizeof work);
-//    if (!fr) return 1;
+    //fr = f_mkfs("", &opts, work, sizeof work);
+	 //printf("f_mkfs result: %d\n", fr);
+    //if (fr != 0) return 1;
 
 	 printf("Mounting filesystem\n");
     /* Give a work area to the default drive */
@@ -39,14 +46,20 @@ int main(int argc, char *argv[]){
 
     f_close(&fil);
 	 */
-
+for (uint32_t i = 0; i < 250; i++) {
+	
 	 printf("Create newe.txt\n");
-    fr = f_open(&fil, "newe.txt", FA_CREATE_ALWAYS | FA_WRITE | FA_READ);
+	 char fname[] = "00000.txt";
+	 uint8_t nullchar = _uint32_to_str(i, fname);
+	 fname[nullchar] = '0';
+    fr = f_open(&fil, fname, FA_CREATE_ALWAYS | FA_WRITE | FA_READ);
     if (fr) return (int)fr;
 	 printf("Add content to the file\n");
     f_puts("Specifies write access to the file. Data can be written to the file. Combine with FA_READ for read-write access.\0", &fil);
     f_puts("The input Unicode characters in multiple encoding unit, such as surrogate pair and multi-byte sequence.\n\0", &fil);
     f_close(&fil);
+}
+	 
     /* Open a text file */
     fr = f_open(&fil, "newe.txt", FA_READ);
     if (fr) return (int)fr;
