f61d53e80170dc5e1983485664b957af17de38c7
elpatosilva <elpatosilva@gmail.com>
2024-05-30 15:03:18 -0300

    Updates on seven_segments

---
+++ dev/elec_pot/Core/Inc/button.h
@@ -0,0 +1,13 @@
+/*
+ * button.h
+ *
+ *  Created on: May 30, 2024
+ *      Author: psilva
+ */
+
+#ifndef INC_BUTTON_H_
+#define INC_BUTTON_H_
+
+
+
+#endif /* INC_BUTTON_H_ */
---
+++ dev/elec_pot/Core/Src/button.c
@@ -0,0 +1,9 @@
+/*
+ * button.c
+ *
+ *  Created on: May 30, 2024
+ *      Author: psilva
+ */
+
+
+//void sseg_init(_sseg *s){
