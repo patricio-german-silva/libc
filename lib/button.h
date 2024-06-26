23e17ec056437c1a3271c12452a939d8dd344b66
elpatosilva <elpatosilva@gmail.com>
2024-06-26 11:21:05 -0300

    Added docs

--- lib/button.h
+++ lib/button.h
@@ -3,7 +3,11 @@
  *
  *  Created on: May 30, 2024
  *      Author: psilva
- * Time is in btn_work call period
+ * 
+ * Manages and debounce a button, controls behaviour
+ *
+ * All the Time-based parameters are in btn_work call period
+ *
  */
 
 #include "stdint.h"
@@ -36,12 +40,12 @@ typedef struct{
   uint32_t port;
   uint16_t pin;
   /*
-   * Modo de operacion del boton
+   * Operation mode
    * 0: normal
    * 1: normal, repeat
    * 2: normal, repeat and accel 1
    * 3: normal, repeat and accel 2
-   * 4: multivalue switch mode, 255 values max (multivalue switch with min_value = 0 and max_value = 1 (default) -> normal on/off switch)
+   * 4: multivalue switch mode, 255 values max ( default: multivalue switch with min_value = 0 and max_value = 1 -> normal on/off switch)
    * 5: multivalue switch mode, repeat
    * 6: multivalue switch mode, repeat and accel 1
    * 7: multivalue switch mode, repeat and accel 2
@@ -58,16 +62,39 @@ typedef struct{
 	btn_read_gpio_def btn_read_gpio;
 }_btn;
 
+/* Initialize button, specified mode, port and pin*/
 void btn_init(_btn *b, uint8_t mode, uint32_t port, uint16_t pin);
+
+/* Resets button status */
 void btn_reset(_btn *b);
+
+/* Attachs the read port and pin function */
 void btn_attach_read_gpio(_btn *b, btn_read_gpio_def f);
+
+/* Sets the button mode of operation */
 void btn_set_mode(_btn *b, uint8_t mode);
+
+/* When multivalue shitch mode is specified, defines min value */
 void btn_min_switch_mode_value(_btn *b, uint8_t s);
+
+/* When multivalue shitch mode is specified, defines max value */
 void btn_max_switch_mode_value(_btn *b, uint8_t s);
+
+/* Read button state
+ * If mode of operation is switch, returns the state value
+ * If mode of operation is one of the normals, acknowledge the state (resets the state to 0) */
 uint8_t btn_read_state(_btn *b);
-uint8_t btn_get_state(_btn *b);   // Return state but dont acknowledge
-uint8_t btn_state_changed(_btn *b);     // state changed since de last btn_read_state?
-void btn_write_status(_btn *b, uint8_t status); // Force given status
+
+/* Return state but dont acknowledge */
+uint8_t btn_get_state(_btn *b);
+
+/* Returns 1 if state changed since de last btn_read_state */
+uint8_t btn_state_changed(_btn *b);
+
+/* Force given status on the button and set it as if it was pressed */
+void btn_write_status(_btn *b, uint8_t status);
+
+/* Call it periodicaly, time-based parameters are based on the periods bettween calls to this function*/
 void btn_work(_btn *b);
 
 #endif /* INC_BUTTON_H_ */
--- lib/seven_segments.h
+++ lib/seven_segments.h
@@ -1,6 +1,8 @@
 /*
  * seven_segments.h
  *
+ * Manages a multi-digit seven segments display
+ *
  *  Created on: May 29, 2024
  *      Author: psilva
  */
@@ -10,23 +12,24 @@
 #include "stdint.h"
 #include "strutil.h"
 
-// Cantidad de digitos del display
+// Number of digits on the display
 #define _SSEG_NUMBER_OF_DIGITS 3
 
-// Tipo de display
+// type of display
 //#define _SSEG_DISPLAY_COMMON_ANODE
 #define _SSEG_DISPLAY_COMMON_CATODE
 
-// Informacion a mostrar con titulo
+//  Maximum number of info labels
 #define _SSEG_NUMBER_OF_INFO_LABELS 16
 
-// Tiempo que se muestra el label y la info, en cantidad de llamadas a work
+/* Amount of time to be shown the label and info values
+ * based on the number of calls to the work function */
 #define _SSEG_SHOW_INFO_LABEL_TIME 1000
 #define _SSEG_SHOW_INFO_DATA_TIME 3000
 
 
 
-// Caracteres especiales no mapeables
+// Non-mapeable digits
 #define _SSEG_BLANK_ID 36 // Must match with 'digitCodeMap'
 #define _SSEG_DASH_ID 40
 #define _SSEG_PERIOD_ID 37
@@ -98,28 +101,90 @@ typedef struct{
 	sseg_set_gpio_def sseg_sgpio_low;
 }_sseg;
 
+/* Initialize object*/
 void sseg_init(_sseg *s);
+
+/* Sets port and pin for each led segment (8 total, seven segments plus the dot)*/
 void sseg_set_segments(_sseg *s, const uint32_t *port, const uint16_t *pin);
+
+/* Sets port and pin for each digit, _SSEG_NUMBER_OF_DIGITS total */
 void sseg_set_digits(_sseg *s, const uint32_t *port, const uint16_t *pin);
+
+/* Populate the info labels, max is _SSEG_NUMBER_OF_INFO_LABELS */
 void sseg_set_info_label(_sseg *s, const char *d, uint8_t i);
+
+/* Attaches a function that puts a specific port and pin high */
 void sseg_attach_gpio_high(_sseg *s, sseg_set_gpio_def f);
+
+/* Attaches a function that puts a specific port and pin low */
 void sseg_attach_gpio_low(_sseg *s, sseg_set_gpio_def f);
-void sseg_write_numeric_data(_sseg *s, int32_t n, int8_t dp);				// n: Number to show, dp: decimal places. dp=0 -> no decimal places but dot on the last digit, dp=-1 -> no dot at all
-void sseg_write_char_data(_sseg *s, const char *d);				// n: Number to show, dp: decimal places
-void sseg_write_data_digit(_sseg *s, uint8_t digit, uint8_t value);		// digit, value, on G F E D C B A DP bits format
-void sseg_shift_data_left(_sseg *s);		// shift digits to the left
-void sseg_shift_data_rigth(_sseg *s);		// shift digits to the rigth
-void sseg_write_numeric_info(_sseg *s, int32_t n, int8_t dp, uint8_t l);	// Number to show, decimal places, labbel to show
-void sseg_write_char_info(_sseg *s, const char *d, uint8_t l);	// Number to show, decimal places, labbel to show
-void sseg_update_info(_sseg *s, int32_t n, int8_t dp, uint8_t l, uint8_t ut, uint8_t olt);	// If info is displayed, updates current info, only if label matches. Number to show, decimal places, label, ut:1: update time to show too, olt = 1: override lavel time, star show info immediately
-void sseg_write_info_digit(_sseg *s, uint8_t digit, uint8_t value);		// digit, value, on G F E D C B A DP bits format
-void sseg_shift_info_left(_sseg *s);		// shift digits to the left
-void sseg_shift_info_rigth(_sseg *s);		// shift digits to the rigth
-void sseg_abort_info(_sseg *s);										// Stop showing current info
-void sseg_hold_info(_sseg *s, uint8_t h);							// The display holds the info values 1: true, 0: false
-uint8_t sseg_get_hold_info(_sseg *s);							// Retruns 1 if the display holds the info values
-void sseg_toggle_hold_info(_sseg *s);							// The display holds the info values 1: true, 0: false
-uint8_t sseg_display_status(_sseg *s);							// 0: show data, 1: show info label, 2: show info data
+
+/* Numeric data to be displayed
+ * n: Number to show
+ * dp: decimal places. dp=0 -> no decimal places but dot on the last digit, dp=-1 -> no dot at all
+ */
+void sseg_write_numeric_data(_sseg *s, int32_t n, int8_t dp);
+
+/* Char data to be displayed */
+void sseg_write_char_data(_sseg *s, const char *d);
+
+/* Low level, writes specific segments on specific data digit
+ * digit, value, on G F E D C B A DP bits format
+ */
+void sseg_write_data_digit(_sseg *s, uint8_t digit, uint8_t value);
+
+/* shift digits to the left */
+void sseg_shift_data_left(_sseg *s);	
+
+/* shift digits to the rigth */
+void sseg_shift_data_rigth(_sseg *s);
+
+/* Writes numeric info
+ * Number to show, decimal places, labbel index 
+ */
+void sseg_write_numeric_info(_sseg *s, int32_t n, int8_t dp, uint8_t l);
+
+/* Writes numeric info
+ * Number to show, decimal places, labbel index 
+ */
+void sseg_write_char_info(_sseg *s, const char *d, uint8_t l);
+
+/* If info is displayed, updates current info, only if label matches.
+ * Number to show, decimal places, labeli
+ * ut:1: resets time to _SSEG_SHOW_INFO_DATA_TIME
+ * olt = 1: override label time, showing info immediately
+ */
+void sseg_update_info(_sseg *s, int32_t n, int8_t dp, uint8_t l, uint8_t ut, uint8_t olt);
+
+
+/* Low level, writes specific segments on specific info digit
+ * digit, value, on G F E D C B A DP bits format
+ */
+void sseg_write_info_digit(_sseg *s, uint8_t digit, uint8_t value);
+
+/* shift info digits to the left */
+void sseg_shift_info_left(_sseg *s);
+
+/* shift info digits to the rigth */
+void sseg_shift_info_rigth(_sseg *s);
+
+/* Stop showing current info */
+void sseg_abort_info(_sseg *s);
+
+/* The display holds the info values
+ * 1: true, 0: false */
+void sseg_hold_info(_sseg *s, uint8_t h);
+
+/* Retruns 1 if the display holds the info values */
+uint8_t sseg_get_hold_info(_sseg *s);
+
+/* The display holds the info values 1: true, 0: false */
+void sseg_toggle_hold_info(_sseg *s);	
+
+/* 0: show data, 1: show info label, 2: show info data */
+uint8_t sseg_display_status(_sseg *s);
+
+/* Call periodicaly, all the time-based actions are based on the period between calls to this function */
 void sseg_work(_sseg *s);
 
 
